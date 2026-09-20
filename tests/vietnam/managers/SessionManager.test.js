43da15a9d9a4c4f45893390ea2f4595ec57afb17
Finbar <finbar@finbar.xyz>
2026-09-20 12:49:13 -0400

    refactor: Port SessionManager from ww2 to vietnam

---
+++ tests/vietnam/managers/SessionManager.test.js
@@ -0,0 +1,34 @@
+// Not all methods can be tested as they require active players or disrupt the server.
+// Untested methods: setMap, setSectorLayout, broadcast
+
+const { VietnamClient } = require("../../../src/index.js");
+require("dotenv").config({ quiet: true });
+
+describe("SessionManager", () => {
+  let client;
+
+  beforeAll(async () => {
+    client = new VietnamClient({
+      host: process.env.RCON_HOST,
+      port: process.env.RCON_PORT,
+      password: process.env.RCON_PASSWORD
+    });
+
+    await client.init();
+  });
+
+  afterAll(() => {
+    client.disconnect();
+  });
+
+  describe("fetch", () => {
+    it("should fetch a Session object representing the live game state.", async () => {
+      const response = await client.session.fetch();
+
+      // Ensure the payload parsed correctly into standard camelCase properties
+      expect(response).toHaveProperty("serverName");
+      expect(response).toHaveProperty("mapName");
+      expect(typeof response.playerCount).toBe("number");
+    });
+  });
+});
